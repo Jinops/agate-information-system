786ea96d59f49dd24dec722229d47126664e40eb
Jinwoo Park <46846964+Jinops@users.noreply.github.com>
2022-12-04 15:20:21 +0000

    Add meta data for documentation

--- InformationSystem/agate/main.py
+++ InformationSystem/agate/main.py
@@ -4,7 +4,21 @@ import uvicorn
 from Class import Client
 from Class import Campaign
 
-app = FastAPI()
+tags_metadata = [ 
+  {
+    "name": "clients",
+    "description": "Agate의 고객",
+  },
+  {
+    "name": "campaigns",
+    "description": "광고 활동의 집합",
+  },
+  {
+    "name": "adverts",
+    "description": "개별 광고",
+  },
+] # https://ie.jinwoop.repl.co/docs
+app = FastAPI(openapi_tags=tags_metadata)
 
 
 @app.get("/")
@@ -14,21 +28,25 @@ async def main():
 
 ## client
 
-@app.get("/clients")
+
+@app.get("/clients", tags=["clients"])
 async def get_all_client():
   res = Client.get_all()
   return res
 
 
-@app.get("/clients/{id}")
+@app.get("/clients/{id}", tags=["clients"])
 async def get_client(id: int):
   res = Client.get(id)
   return res
-  
-@app.post("/clients/add")
-async def add_client(req: Client.Client):
+
+
+@app.post("/clients/add", tags=["clients"])
+async def add_client(req: Client.Client, ):
   Client.add(req.staff_id, req.name, req.tel_number)
   return {}
+
+
 '''
 {
   "staff_id":1,
@@ -39,25 +57,32 @@ async def add_client(req: Client.Client):
 
 ## campaign
 
-@app.get("/campaigns")
+
+@app.get("/campaigns", tags=["campaigns"])
 async def get_all_campaign():
   res = Campaign.get_all()
   return res
 
-@app.get("/campaigns/{id}")
+
+@app.get("/campaigns/{id}", tags=["campaigns"])
 async def get_campaign(id: int):
   res = Campaign.get(id)
   return res
 
-@app.get("/campaigns/by_client/{client_id}")
+
+@app.get("/campaigns/by_client/{client_id}", tags=["campaigns"])
 async def get_campaign_by_client(client_id: int):
   res = Campaign.get_list_by_client(client_id)
   return res
 
-@app.post("/campaigns/add")
+
+@app.post("/campaigns/add", tags=["campaigns"])
 async def add_campaign(req: Campaign.Campaign):
-  Campaign.add(req.client_id, req.title, req.advert_id_list, req.start_date, req.end_date)
+  Campaign.add(req.client_id, req.title, req.advert_id_list, req.start_date,
+               req.end_date)
   return "OK"
+
+
 '''
 {
     "client_id": 1,
@@ -80,8 +105,16 @@ async def generate_test_data():
   Client.add(staff_id=3, name="Apple", tel_number="010-0000")
   Client.add(staff_id=4, name="수원시청", tel_number="010-0000")
 
-  Campaign.add(client_id=1, title="고전 캠페인", advert_id_list=[], start_date="2019-01-01", end_date="2019-12-31")
-  Campaign.add(client_id=2, title="상반기 캠페인", advert_id_list=[], start_date="2022-01-01", end_date="2022-05-31")
+  Campaign.add(client_id=1,
+               title="고전 캠페인",
+               advert_id_list=[],
+               start_date="2019-01-01",
+               end_date="2019-12-31")
+  Campaign.add(client_id=2,
+               title="상반기 캠페인",
+               advert_id_list=[],
+               start_date="2022-01-01",
+               end_date="2022-05-31")
   Campaign.add(client_id=2, title="12월 연휴 캠페인", advert_id_list=[])
 
   return "OK"
